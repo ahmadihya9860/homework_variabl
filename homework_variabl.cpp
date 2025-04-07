737df80075b358ba574a7f32696ced3e56e4cb6c
ahmadihya9860 <Ahmadihya9860@gmail.com>
2025-04-07 20:45:43 +0200

    Add files via upload

---
+++ homework_variabl.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name = "ahmad";
+    int age = 17;
+    string city = "agadir";
+    string country = "morroco";
+    int  monthly_salary = 6000;
+    int yearl_salary = 10000;
+    char gender = 'm';
+    bool married=false;
+
+    cout <<"name : "<<name << endl;
+    cout <<"age : " << age << endl;
+    cout << "city : "<<city << endl;
+    cout << "contry : "<<country << endl;
+    cout << " monthly salary : "<< monthly_salary <<endl;
+    cout << "yearl salary : "<<yearl_salary<<endl;
+    cout << "gender : "<<gender<<endl;
+    cout << "married : " << married<<endl;
+
+
+
+}
