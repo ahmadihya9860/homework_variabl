5dd3872ebbc37e52f790ac5b015fac24a55945a2
ahmadihya9860 <ihyaahmad883@gmail.com>
2025-04-08 21:14:02 +0200

    add file cpp

---
+++ calcul.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number_1;
+    int number_2;
+    cout <<"enter number 1"<<endl;
+    cin >>number_1;
+    cout <<"enter number 2"<<endl;
+    cin >>number_2;
+    cout << number_1 << "+"<<number_2<<"=" << number_1 + number_2 <<endl;
+    
+    return 0;
+}
